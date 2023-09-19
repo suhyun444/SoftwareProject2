9aedbfcdce3ce6345f179cdd2ffa3fce8cacfd56
Suhyun <67677286+suhyun444@users.noreply.github.com>
2023-09-19 14:06:04 +0900

    Add 05P12 assignment

---
+++ 05_practice_2.ino
@@ -0,0 +1,20 @@
+#define PIN_LED 7
+
+void setup() {
+    pinMode(PIN_LED,OUTPUT);
+    Serial.begin(115200);
+    while(!Serial);
+    digitalWrite(PIN_LED,0);
+}
+void loop() {
+    delay(1000);
+    for(int i=0;i<5;++i)
+    {
+      digitalWrite(PIN_LED,1);
+      delay(100);
+      digitalWrite(PIN_LED,0); 
+      delay(100);
+    }
+    digitalWrite(PIN_LED,1);
+    while(1);
+}
